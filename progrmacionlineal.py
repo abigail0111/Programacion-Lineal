97ed4bb4638a10e6ad25f69a20c11f52c47c9376
VeraniaHdez12 <47038209+VeraniaHdez12@users.noreply.github.com>
2019-02-15 12:20:52 -0600

    Update progrmacionlineal.py

--- progrmacionlineal.py
+++ progrmacionlineal.py
@@ -1,15 +1,15 @@
 from pulp import *
 
 x = LpVariable("x", 0, 3)
-y = LpVariable("y", 0, 1)
-
-prob = LpProblem("myProblem", LpMinimize)
+y = LpVariable("y", 0, 1)-lu0kṕ.pl,]_
+prob = LpProblem("myProblem", LpMinim}
+                 ol¿'mik,'0 ize)
 prob += x + y <= 2
 
 
 prob += -4*x + y
 
-status=prob.solve()
+stat m,}lip{.lkþ.{-Lł[us=prob.solve()
 
 print(value(x), value(y))
 
